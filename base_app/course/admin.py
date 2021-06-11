a989b9c455d7b1a15f65a39cf10662627ee654d5
andrewzweb <andrewzweb@gmail.com>
2021-06-11 16:17:12 +0300

    Add admin view to coursec, student, pacticipant

--- base_app/course/admin.py
+++ base_app/course/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Course
 
-# Register your models here.
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    '''course admin'''
+    
+    list_display = (
+        'name',
+        'description',
+        'start_date',
+        'end_date'
+    )
+
+    search_fields = ('name', 'description')
+    list_filter = ('start_date', 'end_date')
--- base_app/participant/admin.py
+++ base_app/participant/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import CourseParticipant
 
-# Register your models here.
+
+@admin.register(CourseParticipant)
+class CourseParticipantAdmin(admin.ModelAdmin):
+    '''course participant admin'''
+    
+    list_display = (
+        'id',
+        'course',
+        'student',
+        'completed'
+    )
+
+    search_fields = ('course', 'student')
+    list_filter = ('course', 'completed')
